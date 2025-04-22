ccbd1cd99b2f80bce8e04b55bb420de870a14cec
divsingh14 <divsingh14@yahoo.in>
2025-04-22 08:51:02 +0530

    Test

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose.connect(
-    "mongodb+srv://divsingh14:Test123@namastenode.lehe1o7.mongodb.net/UserCRUD?retryWrites=true&w=majority&appName=NamasteNode"
+    ""
   );
 };
 
